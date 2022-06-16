4694ab512b01a4b9ef40686e207c36c21459090c
Bhargav Rudhra <bhargav.rudhra@rampgroup.com>
2022-06-16 13:04:48 +0530

    added callbackfunction file added

---
+++ callbackfunction/Callback.js
@@ -0,0 +1,10 @@
+function person(friend, person2) {
+  setTimeout(function () {
+    alert("My friend's name is" + friend);
+    person2();
+  });
+}
+function person2() {
+  alert("My name is Bhargav");
+}
+person("Rudhra", person2);
