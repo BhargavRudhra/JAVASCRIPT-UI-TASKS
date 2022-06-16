9086f58261ed1ed62330bc1ada1766c12b15e982
Bhargav Rudhra <bhargav.rudhra@rampgroup.com>
2022-06-16 16:05:17 +0530

    made changes in all files for value names and functioning

--- Api-data-display/Apidata.js
+++ Api-data-display/Apidata.js
@@ -1,6 +1,5 @@
 fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then((res) => {
   res.json().then((data) => {
-    console.log(data.drinks);
     if (data.drinks.length > 0) {
       var temp = "";
       data.drinks.forEach((itemData) => {
@@ -9,7 +8,7 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php").then((res) => {
         temp += "<td>" + itemData.strDrink + "</td>";
         temp += "<td>" + itemData.strCategory + "</td></tr>";
       });
-      document.getElementById("data").innerHTML = temp;
+      document.getElementById("resultdata").innerHTML = temp;
     }
   });
 });
